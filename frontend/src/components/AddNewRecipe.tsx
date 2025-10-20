da5466b2840c537d6e20542e68cb7030be44fd3f
Nasha <ignacia.galaz@ug.uchile.cl>
2025-10-20 11:35:05 -0300

    feat: se integra el formulario para agregar recetas con el resto de la página - Falta incluir backend - Falta mejorar el estilo

---
+++ frontend/src/components/AddNewRecipe.tsx
@@ -0,0 +1,15 @@
+import Footer from "./Footer"
+import Navbar from "./Navbar"
+import NewRecipeForm from "./NewRecipeForm"
+
+const AddNewRecipe = () => {
+    return (
+        <div>
+            <Navbar />
+            <NewRecipeForm></NewRecipeForm>
+            <Footer />
+        </div>
+    )
+}
+
+export default AddNewRecipe
