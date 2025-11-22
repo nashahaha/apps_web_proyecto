b4a9ca156a595ecc0ce29d84b44ea8e0cac47e13
Gabriela Urbina <111938293+gurbina2234@users.noreply.github.com>
2025-11-21 21:58:11 -0300

    feat modularidad codigo

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,28 +1,5 @@
 import { test, expect } from "@playwright/test";
-async function login(page) {
-    await page.goto("/");
-    await page.getByRole("link", { name: "Login" }).click();
-
-    await page.getByLabel("Email").fill("test@example.com");
-    await page.getByLabel("Password").fill("123456");
-
-    await page.getByRole("button", { name: /login/i }).click();
-    await expect(page.getByRole("heading", { name: /ñom ñom/i })).toBeVisible();
-}
-
-async function registerDefaultUser(request) {
-    // resetea DB
-    await request.post("http://localhost:3001/api/testing/reset");
-
-    // crea usuario
-    await request.post("http://localhost:3001/api/auth/register", {
-        data: {
-            name: "Test User",
-            email: "test@example.com",
-            password: "123456",
-        },
-    });
-}
+import { login, registerDefaultUser } from "../helpers/auth";
 
 test.describe("Authentication", () => {
     test.beforeEach(async ({ request }) => {
